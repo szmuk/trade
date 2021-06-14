c46ce09438afa4c3701cf1eb1c1ed95aa29ef7a6
Adam Szmukała <adam.szmukala@verve-capital.com>
2021-06-14 14:12:37 +0200

    small adjustments

--- trade/src/app/core/services/people/people.serice.ts
+++ trade/src/app/core/services/people/people.serice.ts
@@ -22,36 +22,68 @@ export class PeopleService {
       avgTradeTime: 18
     },
     style: ['Lotto', 'Risky', 'Aggresive', 'Day trades', 'Penny stocks'],
-    alerts: [{
-      id: 2,
-      company: {
-        id: 3,
-        code: 'SPY',
-        name: 'S&P 500 Index',
-        imageUrl: 'spy.png'
-      },
-      user: {
+    alerts: [
+      {
         id: 2,
-        name: 'Adam',
-        login: 'szmuk',
-        imageUrl: 'p2.jpg',
-        alertsCount: 38,
-        following: false
+        company: {
+          id: 3,
+          code: 'SPY',
+          name: 'S&P 500 Index',
+          imageUrl: 'spy.png'
+        },
+        user: {
+          id: 2,
+          name: 'Adam',
+          login: 'szmuk',
+          imageUrl: 'p2.jpg',
+          alertsCount: 38,
+          following: false
+        },
+        closed: null,
+        opened: moment().subtract(1, 'days').toDate(),
+        type: 'Day',
+        gainLoss: 6,
+        likesCount: 14,
+        savedCount: 2,
+        commentsCount: 7,
+        top: false,
+        liked: false,
+        saved: false,
+        commented: false,
+        price: 110.22,
+        currency: '$'
       },
-      closed: null,
-      opened: moment().subtract(1, 'days').toDate(),
-      type: 'Day',
-      gainLoss: 6,
-      likesCount: 14,
-      savedCount: 2,
-      commentsCount: 7,
-      top: false,
-      liked: false,
-      saved: false,
-      commented: false,
-      price: 110.22,
-      currency: '$'
-    }]
+      {
+        id: 3,
+        company: {
+          id: 2,
+          code: 'TSLA',
+          name: 'Tesla Inc',
+          imageUrl: 'tesla.png'
+        },
+        user: {
+          id: 2,
+          name: 'Adam',
+          login: 'szmuk',
+          imageUrl: 'p2.jpg',
+          alertsCount: 38,
+          following: false
+        },
+        closed: moment().subtract(1, 'days').toDate(),
+        opened: moment().subtract(2, 'days').toDate(),
+        type: 'Day',
+        gainLoss: -12,
+        likesCount: 14,
+        savedCount: 2,
+        commentsCount: 7,
+        top: false,
+        liked: false,
+        saved: true,
+        commented: false,
+        price: 110.36,
+        currency: '$'
+      }
+    ]
   };
 
   constructor() {
